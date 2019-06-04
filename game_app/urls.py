1ba803b9f0ccbd17cd77fda45104975015711d9a
BrightX <47632184+BrightX@users.noreply.github.com>
2019-06-05 00:06:30 +0800

    Add files via upload by Pycharm.

--- game_app/urls.py
+++ game_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.game),
     path('Snake/', views.snake),
-    path('StreetFighter/', views.street_fighter),
     path('Minesweeper/', views.minesweeper),
     path('Study/', views.study),
 ]
